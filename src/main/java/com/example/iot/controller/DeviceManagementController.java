d444d52aa89c01c23dbf9bd55449cb0cf6f19519
Debonet <719893090@qq.com>
2020-06-19 21:35:24 +0800

    整合

--- src/main/java/com/example/iot/controller/DeviceManagementController.java
+++ src/main/java/com/example/iot/controller/DeviceManagementController.java
@@ -1,23 +1,30 @@
 package com.example.iot.controller;
 
 import com.example.iot.service.DeviceManagementService;
+import com.example.iot.vo.DeviceVO;
 import com.example.iot.vo.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/device")
+@RequestMapping("api/device")
 public class DeviceManagementController {
     @Autowired
     DeviceManagementService deviceManagementService;
 
-    @RequestMapping("/addDevice")
-    public int addDevice(@RequestParam(value = "type",required = true) String type, @RequestParam(value = "owner",required = true) String owner){
-        return deviceManagementService.addDevice(type, owner);
+    @CrossOrigin
+    @PostMapping(value = "/addDevice")
+    @ResponseBody
+    public int addDevice(@RequestBody DeviceVO deviceVO){
+        return deviceManagementService.addDevice(deviceVO.getType(), deviceVO.getUsername());
     }
+
+//    public int addDevice(@RequestParam(value = "type",required = true) String type, @RequestParam(value = "owner",required = true) String owner){
+//        return deviceManagementService.addDevice(type, owner);
+//    }
+
     @RequestMapping("/deleteDevice")
     public Response deleteDevice(@RequestParam(value ="deviceId",required = true)String deviceId){
         boolean result=deviceManagementService.deleteDevice(deviceId);
