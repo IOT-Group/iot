97d6b3982768e7cfc36a7d684a7487ac60177b8d
BensonChou <2027178562@qq.com>
2020-06-19 21:57:26 +0800

    test

--- src/main/java/com/example/iot/controller/CalendarController.java
+++ src/main/java/com/example/iot/controller/CalendarController.java
@@ -3,19 +3,19 @@ package com.example.iot.controller;
 import com.example.iot.service.CalendarService;
 import com.example.iot.vo.Operation;
 import com.example.iot.vo.Response;
+import com.example.iot.vo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("api/calendar")
 public class CalendarController {
     @Autowired
     CalendarService calendarService;
 
     @CrossOrigin
-    @PostMapping(value = "/addCalendar")
+    @PostMapping(value = "api/addCalendar")
     @ResponseBody
     public Response addCalendar(@RequestBody Operation operation){
         boolean result= calendarService.addCalendar(operation.getDeviceId(),operation.getTime(),operation.getCode());
@@ -23,9 +23,16 @@ public class CalendarController {
     }
 
     @CrossOrigin
-    @PostMapping(value = "/deleteCalendar")
+    @PostMapping(value = "api/deleteCalendar")
     @ResponseBody
     public void deleteCalendar(@RequestBody Operation operation) {
         calendarService.deleteCalendar(operation.getDeviceId(),operation.getTime(),operation.getCode());
+    }
+
+    @CrossOrigin
+    @PostMapping(value = "api/deleteCalendar")
+    @ResponseBody
+    public void showCalendar(@RequestBody User requestUser){
+
     }
 }
--- src/main/java/com/example/iot/dao/Repository/CalendarRepository.java
+++ src/main/java/com/example/iot/dao/Repository/CalendarRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 public interface CalendarRepository {
     public abstract boolean addCalendar(String deviceId,String time,String code);
     public abstract void deleteCalendar(String deviceId,String time,String code);
+    //public abstract
 }
