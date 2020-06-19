bc0020ef03969554b2128c6edf2ac7130aa8f277
BensonChou <2027178562@qq.com>
2020-06-19 21:37:17 +0800

    test

---
+++ src/test/java/com/example/iot/dao/UserDaoTest.java
@@ -0,0 +1,21 @@
+package com.example.iot.dao;
+
+import com.example.iot.dao.Repository.UserRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserDaoTest {
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    public void registerTest(){
+        userRepository.register("root","root");
+    }
+}
