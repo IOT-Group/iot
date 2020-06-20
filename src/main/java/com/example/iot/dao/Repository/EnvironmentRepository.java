55c2c4bb710e43c91a44e6cae7f5e7dcbc6157fe
BensonChou <2027178562@qq.com>
2020-06-20 14:11:40 +0800

    注册后初始化环境

--- src/main/java/com/example/iot/dao/Repository/EnvironmentRepository.java
+++ src/main/java/com/example/iot/dao/Repository/EnvironmentRepository.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EnvironmentRepository {
+    public void addEnvironment(int userid,int degree,int humidity,int ownerState,int time);
     public void changeDegree(int id,int degree);
     public void changeHumidity(int id,int humidity);
     public void changeHome(int id,int ownerState);
