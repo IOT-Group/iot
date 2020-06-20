56fbbdab470338fb3984516e8df053484f0a4dce
BensonChou <2027178562@qq.com>
2020-06-20 17:08:45 +0800

    test

--- src/main/java/com/example/iot/service/UserService.java
+++ src/main/java/com/example/iot/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.iot.service;
 
+import com.example.iot.dao.Repository.DeviceManagementRepository;
 import com.example.iot.dao.Repository.UserRepository;
 import com.example.iot.dao.UserDao;
 import com.example.iot.po.User.Device;
@@ -18,12 +19,18 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    DeviceManagementRepository deviceManagementRepository;
+
     public boolean register(String username,String password){
         return userRepository.register(username,password);
     }
 
     public boolean login(String username,String password){
-        return userRepository.login(username,password);
+        boolean result= userRepository.login(username,password);
+        if(result)
+            deviceManagementRepository.initialize();
+        return result;
     }
 
     public List<Device> getDevices(String username){
