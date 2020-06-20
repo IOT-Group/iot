72f9157d202b8c795213df22fde6f6f015d879a7
patrickyu <171250521@smail.nju.edu.cn>
2020-06-20 14:55:31 +0800

    final2-ybw

--- src/main/java/com/example/iot/controller/EnvironmentController.java
+++ src/main/java/com/example/iot/controller/EnvironmentController.java
@@ -3,6 +3,7 @@ package com.example.iot.controller;
 import com.example.iot.service.Environment.ChangeEnv;
 import com.example.iot.vo.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,5 +20,9 @@ public class EnvironmentController {
     }
 
     //提供查询环境的接口
+    @RequestMapping("/get")
+    public Response getEnv(@RequestParam(value = "username",required = true)String username){
+        return Response.ResponseSuccess(getEnv(username));
+    }
 
 }
--- src/main/java/com/example/iot/service/Environment/ChangeEnv.java
+++ src/main/java/com/example/iot/service/Environment/ChangeEnv.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ChangeEnv {
     public Environment analyseInpput(String username, String type, String ins);
+    public Environment getEnv(String username);
 }
